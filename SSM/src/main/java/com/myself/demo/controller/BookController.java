0b54d73f24eb713578f150148d20daf1679ae06b
Jarvis1217 <45352073+Jarvis1217@users.noreply.github.com>
2021-01-25 22:04:16 +0800

    Add files via upload

--- SSM/src/main/java/com/myself/demo/controller/BookController.java
+++ SSM/src/main/java/com/myself/demo/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.myself.demo.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.myself.demo.pojo.Books;
 import com.myself.demo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -25,9 +27,13 @@ public class BookController {
         return "allbook";
     }
 
-    @RequestMapping(value = "/test",produces = "application/json;charset=utf-8")
+    @RequestMapping("/test")
     @ResponseBody
     public String test() {
-        return "TestPage,你好世界";
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("test1","测试文本");
+        map.put("test2","测试数据");
+        map.put("test3","测试内容");
+        return JSON.toJSONString(map);
     }
 }
